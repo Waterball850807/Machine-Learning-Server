d23f5db0688098141d5c22c50066d72eeeceabfd
johnny850807 <johnny850807@gmail.com>
2018-05-20 23:34:00 +0800

    init

---
+++ jieba_utils.py
@@ -0,0 +1,24 @@
+import jieba
+
+puncts = set(u''':!),.:;?]}¢'"、。〉》」』】〕〗〞︰︱︳﹐､﹒
+﹔﹕﹖﹗﹚﹜﹞！），．：；？｜｝︴︶︸︺︼︾﹀﹂﹄﹏､～￠
+々‖•·ˇˉ―--′’”([{£¥'"‵〈《「『【〔〖（［｛￡￥〝︵︷︹︻
+︽︿﹁﹃﹙﹛﹝（｛“‘-—_…''')
+
+stop_wordset = puncts
+
+print("Preparing jieba utils...")
+
+jieba.set_dictionary('dict.txt.big')
+
+with open('stop_words.txt', 'r', encoding='utf-8') as f:
+    for stop_word in f:
+        stop_wordset.add(stop_word.strip('\n'))
+
+print("Jieba utils prepared.")
+
+
+def cut(sentence, cut_all=False):
+    return [word for word in jieba.cut(sentence, cut_all=cut_all)
+            if len(word) != 0
+            and word not in stop_wordset]
